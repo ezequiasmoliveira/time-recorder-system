982bdf32d6ac904f74a4fc725ba8bf34f24bf347
Ezequias Monteiro de Oliveira <esequias.m.oliveira@gmail.com>
2018-06-08 10:37:21 -0300

    criação do projeto

---
+++ src/main/java/com/timerecordersystem/TimeRecorderSystemApplication.java
@@ -0,0 +1,12 @@
+package com.timerecordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeRecorderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeRecorderSystemApplication.class, args);
+	}
+}
