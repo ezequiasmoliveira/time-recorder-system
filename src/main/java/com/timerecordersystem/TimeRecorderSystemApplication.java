8a616a0daacedf2ecb9faff64196da8ec0b83991
esequias.m.oliveira <esequias.m.oliveira@gmail.com>
2018-06-09 14:33:05 -0300

    implementação relizada para que o jpa possa converter o uso das classes do package java.time no mapeamento com o banco de dados.

--- src/main/java/com/timerecordersystem/TimeRecorderSystemApplication.java
+++ src/main/java/com/timerecordersystem/TimeRecorderSystemApplication.java
@@ -2,7 +2,13 @@ package com.timerecordersystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
+@EntityScan(basePackageClasses = { 
+		TimeRecorderSystemApplication.class, 
+		Jsr310JpaConverters.class 
+		})
 @SpringBootApplication
 public class TimeRecorderSystemApplication {
 
